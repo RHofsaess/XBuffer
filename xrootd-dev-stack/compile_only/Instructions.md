4262879453fd9ebc762fa5fe837c0cc6606b6be4
RHofsaess <Robin.Hofsaess@kit.edu>
2024-06-28 14:15:57 +0200

    [xrootd-dev-stack] [compile only] More documentation + xrdcp path added

--- xrootd-dev-stack/compile_only/Instructions.md
+++ xrootd-dev-stack/compile_only/Instructions.md
@@ -4,3 +4,5 @@ This docker stack is compiling xrootd using an Alma9 container provided from `rh
 2) Compile with: `$ docker compose up`
 3) If something should be done after compiling, it can be added in `../scripts/build.sh`.
 4) If you want to test the compiled version as a client, have a look into the `client` folder.
+
+**Note: If you want to use a different branch than main, you need to add the according checkout to the `setup.sh`!**
--- xrootd-dev-stack/scripts/client_compiled.sh
+++ xrootd-dev-stack/scripts/client_compiled.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This script sets up logging and starts a client container with the compiled xrootd version
 
-touch /logs/client_compiled.log
-echo "START: $(date) ----------------------------" > /logs/client.log
+touch client_compiled.log
+echo "START: $(date) ----------------------------" > client_compiled.log
 
 # wait for startup of other containers
 sleep 1000
 
 # transfer a file with:
-# XRD_CPUSEPGWRTRD=0 /build/src/xrootd -d 2 -f root://REDIRECTOR:1094//FILE.root . &> /logs/client.log
+# XRD_CPUSEPGWRTRD=0 /build/src/XrdCl/xrdcp -d 2 -f root://REDIRECTOR:1094//FILE.root . &> client_compiled.log
