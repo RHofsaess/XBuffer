2856090876e1bbe870389e888b24d8f5b5f4f5e1
Robin Hofsaess <Robin.Hofsaess@gmx.de>
2024-11-25 20:27:32 +0100

    [Monitoring] run_checks.sh reworked.

--- scripts/monitoring_and_reporting/run_checks.sh
+++ scripts/monitoring_and_reporting/run_checks.sh
@@ -3,24 +3,39 @@
 # This script is used to regularily check on    #
 # the instance and xrootd caching proxy. If one #
 # of them is not running, the script automati-  #
-# cally restarts the setup. The script is 	    #
+# cally restarts the setup. The script is       #
 # started from the management node  every 15    #
 # minutes and reports to the new HF4 module.    #
-#	                        					#
-# ++++++++++  Why is it not python? +++++++++++ #
-# After longer discussion, we decided to go 	#
-# with plain bash to achieve a more     		#
-# generaliable setup that works without any 	#
-# further requirements.			            	#
+#                                               #
+# ++++++++++ Why is it not python? +++++++++++  #
+# After longer discussion, we decided to go     #
+# with plain bash to achieve a more             #
+# general setup that works without any          #
+# further requirements.                         #
 #################################################
 
 # +++++ THINGS TO ADAPT +++++
-WORKDIR=/path/to/basedir # No relative paths!
+WORKDIR=/path/to/basedir  # No relative paths!
 PROXY=x509up_u123456
-STARTSCRIPT=start_XBuffer_instance.sh # start_XBuffer_instance_with_monit.sh
+STARTSCRIPT=start_XBuffer_instance.sh  # start_XBuffer_instance_with_monit.sh
+INSTANCE=proxy
+RESTART=0  # 0:False, 1:True
 # +++++++++++++++++++++++++++
 now=$(date +%y%m%d)
 echo "[$(date)]: +++++++ Starting Check +++++++" >> $WORKDIR/logs/reporting/${now}.log
+
+############### General Information ###############
+get_hostname() {
+	echo "[$(date)]: Running on: $(hostname)" >> $WORKDIR/logs/reporting/${now}.log
+	echo $(hostname)
+
+}
+
+get_timestamp() {
+	timestamp=$(date +"%Y-%m-%dT%H:%M:%S")+01:00
+	echo "[$(date)]: Timestamp: ${timestamp}" >> $WORKDIR/logs/reporting/${now}.log
+	echo ${timestamp}
+}
 
 ############### VOMS Proxy ###############
 
@@ -30,7 +45,7 @@ voms_exported() {
     echo "[$(date)]: Check X509_USER_PROXY set" >> $WORKDIR/logs/reporting/${now}.log
     local expected_value="X509_USER_PROXY=/proxy/${PROXY}"
 
-    command_output=$(apptainer exec instance://proxy /bin/bash -c 'task=$(ps aux | grep -v grep | grep xrootd | awk '\''{print $2}'\''); grep -ao "X509_USER_PROXY=/proxy/${PROXY}" /proc/${task}/task/${task}/environ')  # Note: on the host, the pid differs from the container
+    command_output=$(apptainer exec instance://proxy /bin/bash -c 'task=$(ps aux | grep -v grep | grep xrootd | awk '\''{print $2}'\''); grep -ao "X509_USER_PROXY=/proxy/x509up_u232883" /proc/${task}/task/${task}/environ')  # Note: on the host, the pid differs from the container
 
     # Check if the variable is set and equals the expected value
     if [ "${command_output}" == "$expected_value" ]; then
@@ -46,7 +61,7 @@ voms_remaining() {
     # function to check remaining time of voms proxy
     # return the remaining time in seconds
     echo "[$(date)]: Check remaining time of voms proxy" >> $WORKDIR/logs/reporting/${now}.log
-    command_output=$(apptainer exec instance://proxy /bin/bash -c '/usr/bin/voms-proxy-info --file /proxy/${PROXY} | grep timeleft | awk '\''{print $3}'\')
+    command_output=$(apptainer exec instance://proxy /bin/bash -c 'PROXY=x509up_u232883 /usr/bin/voms-proxy-info --file /proxy/x509up_u232883 | grep timeleft | awk '\''{print $3}'\')
     echo -e "\tTime left: ${command_output}" >> $WORKDIR/logs/reporting/${now}.log
     IFS=":" read -r hours minutes seconds <<< "${command_output}"
 
@@ -58,56 +73,59 @@ voms_remaining() {
     echo $total_seconds
 }
 
-############### Instance and Cache ###############
+############### Instance and Caching Proxy ###############
 
 instance_running() {
     # function to check, if the instance is running
     # CURRENTLY, THE ASSUMPTION IS THAT ONLY ONE INSTANCE IS RUNNING!!!
-    # returns 0:not running, 1:running, 2: status "restarted"
+    # returns 0:running, 1:not running, 2: status "restarted"
     # NOTE: It can be that the caching proxy died but the instance is still running! Therefore, the second check
     echo "[$(date)]: Check, if apptainer instance is running:" >> $WORKDIR/logs/reporting/${now}.log
 
-    # first check, if an instance is running:
+    # first check, if an instance is running
     instance=$(apptainer instance list | awk 'NR>1 {print $1}')
     if [ -z "${instance}" ]; then
         echo -e "\tNo instance found!" >> $WORKDIR/logs/reporting/${now}.log
         # self-healing mechanism:
-        echo -e "\t[$(date)]: Restarting instance and proxy..." >> $WORKDIR/logs/reporting/${now}.log
-        nohup $WORKDIR/scripts/${STARTSCRIPT} >> $WORKDIR/logs/reporting/${now}.log 2>&1 &
-	sleep 8  # wait for the instance to come up
-
-        instance_restarted=$(apptainer instance list | awk 'NR>1 {print $1}')
-        if [ -n "${instance_restarted}" ]; then
-            echo -e "\t[$(date)]: Instance successfully restarted!" >> $WORKDIR/logs/reporting/${now}.log
-            echo "2"
-        else
-            echo -e "\t[ERROR]: Instance restart failed!" >> $WORKDIR/logs/reporting/${now}.log
-            echo "0"
-        fi
-    else
-        command_output=$(apptainer exec instance://proxy /bin/bash -c 'ps aux | grep -v grep | grep xrootd')  # check, if caching proxy is running
-        if [ -n "${command_output}" ]; then
-            echo -e "\tInstance '${instance}' and proxy '${command_output}' running." >> $WORKDIR/logs/reporting/${now}.log
-            echo "1"
-        else
-            echo -e "\t[ERROR] Instance '${instance}' running, but no caching proxy found!" >> $WORKDIR/logs/reporting/${now}.log
-	    echo -e "\t[$(date)]: Restarting instance and proxy..." >> $WORKDIR/logs/reporting/${now}.log
-	    nohup ../${STARTSCRIPT} >> $WORKDIR/logs/reporting/${now}.log 2>&1 &
+        if [[ ${RESTART} -eq 1 ]]; then
+            echo -e "\t[$(date)]: Restarting instance and proxy..." >> $WORKDIR/logs/reporting/${now}.log
+            nohup $WORKDIR/scripts/${STARTSCRIPT} >> $WORKDIR/logs/reporting/${now}.log 2>&1 &
 	    sleep 8  # wait for the instance to come up
 
             instance_restarted=$(apptainer instance list | awk 'NR>1 {print $1}')
-		if [ -n "${instance_restarted}" ]; then
-		    echo -e "\t[$(date)]: Instance successfully restarted!" >> $WORKDIR/logs/reporting/${now}.log
-		    echo "2"
-		else
-		    echo -e "\t[ERROR]: Instance restart failed!" >> $WORKDIR/logs/reporting/${now}.log
-		    echo "0"
-		fi
+            if [ -n "${instance_restarted}" ]; then
+                echo -e "\t[$(date)]: Instance successfully restarted!" >> $WORKDIR/logs/reporting/${now}.log
+                echo "2"
+            else
+                echo -e "\t[ERROR]: Instance restart failed!" >> $WORKDIR/logs/reporting/${now}.log
+                echo "1"
+            fi
         fi
+    fi
+    echo -e "\t[$(date)]: Instance running." >> $WORKDIR/logs/reporting/${now}.log
+    echo "0"
+}
+
+cachingproxy_running() {
+    # function to check, if instance is running
+    # NOTE: This function should only run after instance_running()
+    command_output=$(apptainer exec instance://proxy /bin/bash -c 'ps aux | grep -v grep | grep xrootd')  # check, if caching proxy is running
+    if [ -n "${command_output}" ]; then
+        echo -e "\tInstance and proxy '${command_output}' running." >> $WORKDIR/logs/reporting/${now}.log  # both are running, since the check only works when the instance is running!
+        echo "0"
+    else
+        echo -e "\t[ERROR] No caching proxy found!" >> $WORKDIR/logs/reporting/${now}.log
+	if [[ $RESTART -eq 1 ]]; then
+            echo -e "\t[$(date)]: Restarting instance and proxy..." >> $WORKDIR/logs/reporting/${now}.log
+            nohup $WORKDIR/scripts/${STARTSCRIPT} >> $WORKDIR/logs/reporting/${now}.log 2>&1 &
+            sleep 8  # wait for the instance to come up
+	    echo "2"
+        fi
+	echo "1"
     fi
 }
 
-############### Fil state of Cache ###############
+############### Fill State of Cache ###############
 get_fill_state() {
     # NOTE: +++++ THIS NEEDS TO BE CHANGED IN FUTURE XROOTD VERSIONS +++++
     # Function to query the current estimated cache fill state from the proxy log.
@@ -115,37 +133,67 @@ get_fill_state() {
     # returns the fillstate in TB
     echo "[$(date)]: Checking cache fill State:" >> $WORKDIR/logs/reporting/${now}.log 
     command_output=$(apptainer exec instance://proxy /bin/bash -c ' grep "estimated usage by files" /logs/proxy.log | tail -1 | awk '\''{print $11}'\')
+    if [ -z "${command_output}" ]; then
+        command_output=0
+    fi
     echo -e "\tEstimated usage by files: ${command_output} bytes" >> $WORKDIR/logs/reporting/${now}.log
     echo "${command_output}"
 }
 
+############### SLURM Info ###############
+n_running_nodes() {
+    # returns the nummber of currently running nodes
+    command_output=$(squeue --noheader | grep R | wc -l)
+    echo "[$(date)]: Nummer of running nodes: ${command_output}" >> $WORKDIR/logs/reporting/${now}.log
+    echo "${command_output}"
+}
+
+#running_nodes() {
+# Returns list of the nodes running
+#TODO
+#}
+
+next_start() {
+    # returns the start of the next node
+    command_output=$(squeue --start --noheader --format=%S | head -n1)+01:00
+    echo "[$(date)]: Start time for next node: ${command_output}" >> $WORKDIR/logs/reporting/${now}.log
+    echo "${command_output}"
+}
+
+############### Monitoring ###############
+monit_running() {
+    # checks if python3 is running
+    command_output=$(apptainer exec instance://proxy /bin/bash -c 'ps -eo pid,cmd | grep python3 | grep -v grep')
+    if [ -n "${command_output}" ]; then
+        echo -e "[$(date)]: [Monitoring] running: ${command_output}" >> $WORKDIR/logs/reporting/${now}.log
+	echo "1"
+    else
+        echo "0"
+    fi
+}
 
 ############### Future Extensions ###############
 # - mode
 # - running nodes
-# - next node starting
 # - slurm job ids
-
+# - siteconf
 
 # -------------------------------------------------------------
 
-# test1: instance running
-test1=$(instance_running)
-# test2: check if proxy is valid; returns remaining hours
-test2=$(voms_remaining)
-# test3: check if env variable is set
-test3=$(voms_exported)
-# test4: get fill state of cache
-test4=$(get_fill_state)
-
 # Format the output:
 json_output="{"
-json_output+="\"instance_running\": \"$test1\","
-json_output+="\"voms_remaining_s\": \"$test2\","
-json_output+="\"voms_exported\": \"$test3\","
-json_output+="\"cache_fill_state_b\": \"$test4\""
-
+json_output+="\"timestamp\": \"$(get_timestamp)\","
+json_output+="\"hostname\": \"$(get_hostname)\","
+json_output+="\"instance_running\": $(instance_running),"
+json_output+="\"cachingproxy_running\": $(cachingproxy_running),"
+json_output+="\"voms_remaining_s\": $(voms_remaining),"
+json_output+="\"voms_exported\": $(voms_exported),"
+json_output+="\"cache_fill_state_b\": $(get_fill_state),"
+json_output+="\"next_start\": \"$(next_start)\","
+json_output+="\"n_nodes\": $(n_running_nodes),"
+json_output+="\"monit_running\": $(monit_running)"
 json_output+="}"
 
+echo "[$(date)]: Formatted output: ${json_output}" >> $WORKDIR/logs/reporting/${now}.log
 echo $json_output
 
