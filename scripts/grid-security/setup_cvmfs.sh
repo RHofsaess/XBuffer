bf159672739ba7ac2c5442079dee92bb1f69f356
Robin Hofsaess <Robin.Hofsaess@gmx.de>
2025-03-25 00:32:49 +0100

    Fix cvmfs update service

--- scripts/grid-security/setup_cvmfs.sh
+++ scripts/grid-security/setup_cvmfs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Script to set up cvmfs for copying /etc/grid-security
 # We do not configure/deploy a squid here as it is only one download once in a while
-echo "[$(date)]: Updating /etc/grid-security..." >> $BASEDIR/logs/main.log
-echo CHECK CONFIG!!!# TODO: write to log
-cd $BASEDIR/cvmfsexec
+BASEDIR=<Replace>
+echo "[$(date)]: Updating /etc/grid-security..." >> "$BASEDIR"/logs/main.log
+cd "$BASEDIR"/cvmfsexec
 
-./cvmfsexec grid.cern.ch -- cp -r /cvmfs/grid.cern.ch/etc/grid-security $BASEDIR/cvmfs-grid-certs
+./cvmfsexec grid.cern.ch -- cp -r /cvmfs/grid.cern.ch/etc/grid-security "$BASEDIR"/cvmfs-grid-certs
 if [ $? -ne 0 ]; then
-  echo "[$(date)]: Update failed." >> $BASEDIR/logs/main.log
+  echo "[$(date)]: Update failed." >> "$BASEDIR"/logs/main.log
 else
-  echo "[$(date)]: /etc/grid-security updated." >> $BASEDIR/logs/main.log
+  echo "[$(date)]: /etc/grid-security updated." >> "$BASEDIR"/logs/main.log
 fi
--- setup.sh
+++ setup.sh
@@ -12,47 +12,52 @@ BASEDIR=$(pwd)
 echo "Set BASEDIR to $(pwd)"
 sed -i "s|export BASEDIR=.*|export BASEDIR=$BASEDIR|" "$ENV_FILE"
 # ----------------------------
-mkdir -p $BASEDIR/proxy
-mkdir -p $BASEDIR/cvmfs-grid-certs
-mkdir -p $BASEDIR/logs/reporting
-mkdir -p $BASEDIR/logs/slurm-logs  # For slurm based HPC centers
-chmod -R 777 $BASEDIR/logs
+mkdir -p "$BASEDIR"/proxy
+mkdir -p "$BASEDIR"/cvmfs-grid-certs
+mkdir -p "$BASEDIR"/logs/reporting
+mkdir -p "$BASEDIR"/logs/slurm-logs  # For slurm based HPC centers
+chmod -R 777 "$BASEDIR"/logs
 
-touch $BASEDIR/logs/main.log
-echo "[$(date)]: Directories created..." >> $BASEDIR/logs/main.log
+touch "$BASEDIR"/logs/main.log
+echo "[$(date)]: Directories created..." >> "$BASEDIR"/logs/main.log
 
 # Get cvmfsexec
-echo "[$(date)]: Get cvmfsexec and do initial setup..." >> $BASEDIR/logs/main.log
+echo "[$(date)]: Get cvmfsexec and do initial setup..." >> "$BASEDIR"/logs/main.log
 # This is required for updating /etc/grid-security
 git clone https://github.com/cvmfs/cvmfsexec.git
-cd ./cvmfsexec
+cd ./cvmfsexec || exit
 ./makedist -s osg
+cd "$BASEDIR" || exit
 
 # Setup automated update service for /etc/grid-security
-sed -i "s|^ExecStart=.*|ExecStart=${BASEDIR}/scripts/grid-security/setup_cvmfs.sh|" $BASEDIR/scripts/grid-security/gridsecurity.service
+sed -i "s|^ExecStart=.*|ExecStart=${BASEDIR}/scripts/grid-security/setup_cvmfs.sh|" "$BASEDIR"/scripts/grid-security/gridsecurity.service
+sed -i "s|<Replace>|${BASEDIR}|g" setup_script.sh
+# Copy units
+echo "[$(date)]: Copy units..." >> "$BASEDIR"/logs/main.log
+cp "$BASEDIR"/scripts/grid-security/gridsecurity.service ~/.config/systemd/user/
+cp "$BASEDIR"/scripts/grid-security/gridsecurity.timer ~/.config/systemd/user/
 
 # Enable timer
+echo "[$(date)]: Enable CVMFS unit and timer" >> "$BASEDIR"/logs/main.log
 systemctl --user daemon-reload
-systemctl --user enable $BASEDIR/scripts/grid-security/gridsecurity.timer
+systemctl --user enable "$BASEDIR"/scripts/grid-security/gridsecurity.timer
 
 # Get monitoring tool
-echo "[$(date)]: Cloning ifnop..." >> $BASEDIR/logs/main.log
-git clone https://github.com/RHofsaess/ifnop.git >> $BASEDIR/logs/main.log
+echo "[$(date)]: Cloning ifnop..." >> "$BASEDIR"/logs/main.log
+git clone https://github.com/RHofsaess/ifnop.git >> "$BASEDIR"/logs/main.log
 
-echo "[$(date)]: Basic setup done." >> $BASEDIR/logs/main.log
+echo "[$(date)]: Basic setup done." >> "$BASEDIR"/logs/main.log
 
 echo "Next steps:"
 echo "1) Adapt .env"
 echo "2) Configure additional monitoring (OPTIONAL)"
 echo "3) Put a valid VOMS proxy inside ./proxy. NOTE: in this directory must only be one file!!!"
 echo "3) Setup of services:"
-echo "   - Reporting"
 echo "   - CVMFS grid-sec"
+echo "   - Reporting"
 echo "   - "
 echo "   - Optional: I/O node monitoring: adapt ifnop config file"
 echo "3) Create"
-
-
 
 
 
