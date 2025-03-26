5a386698951883ec966a10d5d081acff9eb4cba4
Robin Hofsaess <Robin.Hofsaess@gmx.de>
2025-03-26 23:56:30 +0100

    Extensions to .env and sanity checks for monitoring start script added.

--- simple_setup/start_monitoring.sh
+++ simple_setup/start_monitoring.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #################################################
-# This script starts the monitoring. If it is 	#
-# already running, it is restarted.		          #
+# This script starts the monitoring. If it is   #
+# already running, it is restarted.	            #
 # This starts the IFNOP I/O monitoring!         #
 #################################################
 source ../.env
+
+echo "[$(date)]: Starting additional instance monitoring..." | tee -a "$LOGDIR"/main.log
 
 # Check, if additional monitoring is installed
 if [[ "$ENABLE_MONIT" -eq 0 ]]; then
@@ -13,10 +15,18 @@ if [[ "$ENABLE_MONIT" -eq 0 ]]; then
 fi
 
 if [[ ! -d "$IFNOP_PATH" ]]; then
-    echo "[$(date)]: Monitoring is enabled, but IFNOP is not available. Please validate the configuration or clone it!" | tee -a "$LOGDIR"/main.log
+    echo "[$(date)]: Monitoring is enabled, but IFNOP is not available/set. Please validate the configuration or clone it!" | tee -a "$LOGDIR"/main.log
 fi
 
-echo "[$(date)]: Starting additional instance monitoring..." | tee -a "$LOGDIR"/main.log
+# Check, if instance is running:
+running_instance=$(apptainer instance list | grep -w "^$INSTANCE\b")
+if [[ -n $running_instance ]]; then
+    echo "[$(date)]: Instance >>${INSTANCE}<< found. Starting the I/O monitoring now..." | tee -a "$LOGDIR"/main.log
+else
+    echo "[$(date)]: [ERROR] Instance >>${INSTANCE}<< not found. Please make sure that the XBuffer instance is running." | tee -a "$LOGDIR"/main.log
+    exit 1
+fi
+
 # Check, if monitoring is running
 monit_running=$(apptainer exec instance://proxy /bin/bash -c 'ps aux |grep -v grep | grep "/ifnop/main.py" -q')
 if [[ "$monit_running" -eq 0 ]]; then
